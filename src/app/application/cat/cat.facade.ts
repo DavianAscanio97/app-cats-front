55c027668a462c7e5401a655d3d8572c0b362801
Davian Ascanio <davianascanio1997@gmail.com>
2024-05-20 01:42:06 -0500

    documentación del código

--- src/app/application/cat/cat.facade.ts
+++ src/app/application/cat/cat.facade.ts
@@ -8,15 +8,31 @@ import { CatService } from '../../core/services/cat.service';
 })
 export class CatFacade {
   constructor(@Inject('CatService') private catService: CatService) { }
+  // Constructor que inyecta el servicio CatService utilizando la inyección de dependencias de Angular
 
+  /**
+   * Obtiene una lista de gatos basada en un término de búsqueda.
+   * @param search Término de búsqueda para filtrar la lista de gatos.
+   * @returns Observable que emite una lista de objetos DetailsCat.
+   */
   getCats(search: string): Observable<DetailsCat[]> {
     return this.catService.getCats(search);
   }
 
+  /**
+   * Obtiene los detalles de un gato específico según su ID.
+   * @param id ID del gato cuyos detalles se desean obtener.
+   * @returns Observable que emite un objeto DetailsCat.
+   */
   getForId(id: string): Observable<DetailsCat> {
     return this.catService.getForId(id);
   }
 
+  /**
+   * Obtiene la imagen de un gato específico según su ID.
+   * @param id ID del gato del que se desea obtener la imagen.
+   * @returns Observable que emite un objeto CatImage.
+   */
   getImage(id: string): Observable<CatImage> {
     return this.catService.getImage(id);
   }
